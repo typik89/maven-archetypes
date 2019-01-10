3a93b44610b1cfee5c0a3381a1f2f8156324e3a7
typik <typik89@yandex.ru>
2019-01-10 18:14:30 +0300

    create structure

---
+++ spring-docker-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,23 @@
+package ${package};
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
+public class App{
+	private static Logger LOG = LoggerFactory.getLogger( App.class );
+	
+    public static void main( String[] args ){
+    	try( ClassPathXmlApplicationContext ctx = 
+				new ClassPathXmlApplicationContext( new String[]{"application.xml"} ) ){			
+			new CountDownLatch(1).await();
+		}catch(Exception ex){
+			LOG.error( "Fatal error in starting ", ex );
+		}
+    }
+}
